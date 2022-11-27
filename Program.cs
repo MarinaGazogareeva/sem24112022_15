04946d194dd58de5cedcb035e6e9885022a72ecc
Marina <marina.gazogareeva@gmail.com>
2022-11-27 23:53:03 +0300

    added sem24112022_1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n % m == 0);
+Console.Write("yes");
+else
+Console.Write($"не кратно, остаток {n % m}");
