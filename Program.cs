2a2a74896542e980ba1d342827bc07000b4c720d
Marina <marina.gazogareeva@gmail.com>
2022-11-28 10:09:23 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿Console.Clear();
 Console.Write("Введите число: ");
-int n = new Random().Next(1, 7); 
-if (n = 6 || n = 7);
+if (n > 1 && n < 7);
 Console.Write("yes");
 else
 Console.Write("no");
